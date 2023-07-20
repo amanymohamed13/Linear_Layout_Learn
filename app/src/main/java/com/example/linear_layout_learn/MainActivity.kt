e7836c6b4a3a924f65f88bc2717ef3de3c81cbb5
amanymohamed137 <93156418+amanymohamed137@users.noreply.github.com>
2023-07-20 21:31:32 +0300

    First update to project

---
+++ app/src/main/java/com/example/linear_layout_learn/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.linear_layout_learn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
