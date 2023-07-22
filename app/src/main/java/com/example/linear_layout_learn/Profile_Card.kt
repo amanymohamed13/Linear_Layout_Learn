934247213a285ab5feb953e94f5ef5ac696e67f5
amanymohamed137 <93156418+amanymohamed137@users.noreply.github.com>
2023-07-22 19:41:32 +0300

    add card 3 layout  Profile_Card

---
+++ app/src/main/java/com/example/linear_layout_learn/Profile_Card.kt
@@ -0,0 +1,11 @@
+package com.example.linear_layout_learn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile_Card : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_card)
+    }
+}
